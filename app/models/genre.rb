332060ef7a8f9c3b2af05e37ba49ad115cd2c62e
myrtokonst <myrtokonst@gmail.com>
2019-04-04 16:40:17 +0100

    SLUGS

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,8 @@
+require_relative './concerns/slugifiable'
 class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+  include Slug::InstanceMethods
+  extend Slug::ClassMethods
 end
